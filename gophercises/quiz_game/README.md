29d3cab02a988f4936dcee5a4208df5619146cc8
sergsoares <acad.sergio@gmail.com>
2021-04-21 14:51:57 -0300

    Added inital exercises

---
+++ gophercises/quiz_game/README.md
@@ -0,0 +1,21 @@
+# Quiz game
+
+Challenge: https://courses.calhoun.io/lessons/les_goph_01
+
+## Todo:
+
+- [x] Setup Arquivo Go
+- [x] Read problems.csv
+- [x] Parsing problems.csv readed to data structure
+- [x] Receive input from user
+- [x] Compare inputs
+- [x] Add score in final
+- [x] Cleanup prints
+- [ ] Use variable timer to stop
+- [ ] Receive time as param
+- [ ] Use variable file to problems
+- [ ] Receive other file from parameters
+
+
+
+
---
+++ gophercises/quiz_game/v1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type problem struct {
+	predicate string
+	result int
+}
+
+func main() {
+	csvFile, err := os.Open("problems.csv")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("OK for Open file")
+
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader := bufio.NewScanner(os.Stdin)
+
+	for _, line := range csvLines {
+		converted, err := strconv.Atoi(line[1])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		item := problem{
+			predicate: line[0],
+			result: converted,
+		}
+
+		fmt.Printf("Question %v: ", item.predicate)
+
+		reader.Scan()
+		value, _:= strconv.Atoi(reader.Text())
+
+		if value != item.result {
+			fmt.Println("Wrong Answer")
+		} else {
+			fmt.Println("Correct Answer")
+		}
+	}
+}
