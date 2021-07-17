e8dec9d7d323e69a910de1ac04ea5a9c3aca39ca
sergsoares <acad.sergio@gmail.com>
2021-07-17 12:12:54 -0300

    Working insert inside DB

--- crud-app/console.go
+++ crud-app/console.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"crud-app/middleware"
+	"crud-app/models"
 )
 
 func main() {
+	// testInsertUser()
+	testConnection()
+}
+
+func testConnection() {
 	middleware.CreateConnection()
+}
+
+func testInsertUser() {
+	user := models.User{
+		ID:       1,
+		Name:     "Sergio",
+		Location: "Campo Grande",
+		Age:      27,
+	}
+
+	middleware.InsertUser(user)
 }
--- crud-app/middleware/handlers.go
+++ crud-app/middleware/handlers.go
@@ -1,16 +1,19 @@
 package middleware
 
 import (
+	"crud-app/models"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-type Response struct {
+type response struct {
 	ID      int64  `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
 }
@@ -37,4 +40,48 @@ func CreateConnection() *sql.DB {
 	fmt.Println("Sucessfully connected!!")
 
 	return db
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Context-Type")
+
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable decode body %v", err)
+	}
+
+	insertID := InsertUser(user)
+
+	res := response{
+		ID:      insertID,
+		Message: "User created!",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func InsertUser(user models.User) int64 {
+	db := CreateConnection()
+
+	defer db.Close()
+
+	statement := `INSERT INTO users (name, location, age) VALUES ($1,$2,$3) RETURNING userid`
+
+	var id int64
+
+	err := db.QueryRow(statement, user.Name, user.Location, user.Age).Scan(&id)
+
+	if err != nil {
+		log.Fatalf("Unable to query %v", err)
+	}
+
+	fmt.Println("Inserted record  %v", id)
+
+	return id
 }
