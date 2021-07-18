ac9414fbf5f6572390a31cd8b27c8819112ff70e
sergsoares <acad.sergio@gmail.com>
2021-07-18 16:26:53 -0300

    Created json example

---
+++ everyday-go/cap4/01-basic-json-parse/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type people struct {
+	Number int `json:"number"`
+}
+
+func main() {
+	text := `{"people": [{"craft": "ISS", "name": "Sergey Rizhikov"}, {"craft": "ISS",
+"name": "Andrey Borisenko"}, {"craft": "ISS", "name": "Shane Kimbrough"}, {"craft":
+"ISS", "name": "Oleg Novitskiy"}, {"craft": "ISS", "name": "Thomas Pesquet"}, {"craft":
+"ISS", "name": "Peggy Whitson"}], "message": "success", "number": 6}`
+	textBytes := []byte(text)
+
+	p := people{}
+	err := json.Unmarshal(textBytes, &p)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(p.Number)
+}
