d05347c91a7841c04c1092c85b89c4c6b0ef7f19
sergsoares <acad.sergio@gmail.com>
2021-07-23 13:45:20 -0300

    Stub for http server

--- go-with-tests/http-server/server.go
+++ go-with-tests/http-server/server.go
@@ -6,17 +6,27 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	score := GetPlayerScore(player)
+	score := p.store.GetPlayerScore(player)
 	fmt.Fprint(w, score)
 }
 
-func GetPlayerScore(player string) string {
-	if player == "Pepper" {
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) string
+}
+
+type RealPlayerStore struct{}
+
+func (r RealPlayerStore) GetPlayerScore(name string) string {
+	if name == "Pepper" {
 		return "20"
 	}
-	if player == "Floyd" {
+	if name == "Floyd" {
 		return "10"
 	}
 
--- go-with-tests/http-server/server_test.go
+++ go-with-tests/http-server/server_test.go
@@ -11,21 +11,40 @@ func newGetScoreRequest(name string) *http.Request {
 	return request
 }
 
+type StubPlayerStore struct {
+	scores map[string]string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) string {
+	score := s.scores[name]
+	return score
+}
+
 func TestGetPlayers(t *testing.T) {
+
 	t.Run("return's peppers score", func(t *testing.T) {
+		store := RealPlayerStore{}
+		server := &PlayerServer{&store}
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
+		store := StubPlayerStore{
+			map[string]string{
+				"Pepper": "20",
+				"Floyd":  "10",
+			},
+		}
+		server := &PlayerServer{&store}
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "10")
 	})
@@ -34,6 +53,6 @@ func TestGetPlayers(t *testing.T) {
 func assertResponseBody(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("response body is wrong, got %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
